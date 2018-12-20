5e9220cd72c8223e4ad201d74c992a7d98556db1
bobbyrj <43006555+bobbyrj@users.noreply.github.com>
2018-12-20 14:52:06 -0200

    Create geraRelatorio.java

---
+++ NFsys1/src/Servlet/geraRelatorio.java
@@ -0,0 +1,56 @@
+package Servlet;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import Object.Usuario;
+
+/**
+ * Servlet implementation class insereTroca
+ */
+public class geraRelatorio extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public geraRelatorio() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+    HttpSession session = request.getSession();
+		
+		String relatorio = (String) request.getParameterValues("relatorio")[0];		
+		    
+		try {
+			List<Item> resposta = Usuario.geraRelatorio(relatorio);
+			response.sendRedirect("apresentaRelatorio.jsp");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			response.sendRedirect("erroRelatorio.jsp");
+		} 
+				
+	}
+
+}
